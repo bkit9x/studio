ac8c2d8fe3a62312c59cec98fbcbbea391745347
Kha Nguyễn Hoàng <hkhadev@gmail.com>
2025-07-29 14:40:43 +0000

    tôi vẫn chưa thấy hiện phần Bottom Navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           "min-h-screen bg-background font-body antialiased"
       )}>
         <div className="relative flex min-h-screen flex-col">
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 pb-20">{children}</main>
           <BottomNav />
         </div>
         <Toaster />
